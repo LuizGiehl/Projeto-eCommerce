ea3b4573221fd60ffc2da526efc201010c9a1a30
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2023-09-07 16:36:49 -0300

    função para aumentar a quantidade de produto

--- src/carrinho.js
+++ src/carrinho.js
@@ -1,5 +1,8 @@
 import { catalogo } from "./ultilidades.js";
 
+const idsProdutoCarrinhoComQuantidade = {
+}
+
 function abrirCarrinho() {
     
     document.getElementById("carrinho").style.right = '0px'
@@ -20,16 +23,43 @@ export function inicializarCarrinho() {
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
+function incrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atualizarInformacaoQuantidade(idProduto)
+}
+
+function decrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atualizarInformacaoQuantidade(idProduto)
+}
+
+function atualizarInformacaoQuantidade(idProduto) {
+    document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
 export function adicionarAoCarrinho(idProduto) {
+    if (idProduto in idsProdutoCarrinhoComQuantidade) {
+        incrementarQuantidadeProduto(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1
     const produto = catalogo.find((p) => p.id === idProduto)
-    const cartaoDoProduto = `<article class="produto-carrinho">
-    <button id="remover-produto"><i class="fa-solid fa-circle-xmark"></i></button>
-    <img src="imagens/${produto.nomeArquivoImg}" alt="${produto.nome}">
-    <div>
-        <p>${produto.nome}</p>
-        <p>Tamanho: M</p>
-        <p id="preco">R$${produto.preco}</p>
-    </div>
-</article>`;
+    const cartaoDoProduto = `
+    <article class="produto-carrinho">
+        <button id="remover-produto">
+             <i class="fa-solid fa-circle-xmark"></i>
+        </button>
+        <img src="imagens/${produto.nomeArquivoImg}" alt="${produto.nome}">
+        <div class="info-produto">
+            <p>${produto.nome}</p>
+            <p>Tamanho: M</p>
+            <p id="preco">R$${produto.preco}</p>
+        </div>
+        <div class="botoes-produto">
+            <button>-</button>
+            <p id="quantidade-${produto.id}">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+            <button>+</button>
+        </div>
+    </article>`;
     document.getElementById("produtos-do-carrinho").innerHTML += cartaoDoProduto
 } 
--- src/ultilidades.js
+++ src/ultilidades.js
@@ -1,55 +1,55 @@
  export const catalogo = [
     {
-        id: 1,
+        id: "1",
         nome: "Regata Minimalist Army",
         marca: "Army",
         preco: 109,
         nomeArquivoImg: "product-01.webp"
     },
     {
-        id: 2,
+        id: "2",
         nome: "Camiseta Cotton RD Experience",
         marca: "Army",
         preco: 139,
         nomeArquivoImg: "product-02.webp"
     },
     {
-        id: 3,
+        id: "3",
         nome: "Jaqueta Athleisure Armybr",
         marca: "Army",
         preco: 299,
         nomeArquivoImg: "product-03.webp"
     },
     {
-        id: 4,
+        id: "4",
         nome: "Camiseta Cotton Tag Army",
         marca: "Army",
         preco: 139,
         nomeArquivoImg: "product-04.webp"
     },
     {
-        id: 5,
+        id: "5",
         nome: "Blusão Premium Winter Oversized",
         marca: "Army",
         preco: 290,
         nomeArquivoImg: "product-05.webp"
     },
     {
-        id: 6,
+        id: "6",
         nome: "Calça Athleisure Armybr",
         marca: "Army",
         preco: 259,
         nomeArquivoImg: "product-06.webp"
     },
     {
-        id: 7,
+        id: "7",
         nome: "Calça Jogger Armybr",
         marca: "Army",
         preco: 209,
         nomeArquivoImg: "product-07.jpg"
     },
     {
-        id: 8,
+        id: "8",
         nome: "Camiseta Camo Soldier Black Ops",
         marca: "Army",
         preco: 139,
